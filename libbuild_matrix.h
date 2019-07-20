ea3b58a9ea120bf34a2d865ec1d7eb5e5b5b20ca
rupakrokade <rupakrokade@gmail.com>
2019-07-21 00:58:07 +0530

    cleaning up repository

--- libbuild_matrix.h
+++
@@ -1,8 +0,0 @@
-#ifndef __LIBBUILD_MATRIX_GW_H__
-#define __LIBBUILD_MATRIX_GW_H__
-
-#include "c_gateway_prototype.h"
-
-C_GATEWAY_PROTOTYPE(matrix);
-
-#endif /* __LIBBUILD_MATRIX_GW_H__ */
--- run.sce
+++
@@ -1,8 +0,0 @@
-try exec cleaner.sce
-end
-
-ulink
-ilib_build('build_matrix',['matrix','matrix','csci6'],"sci_matrix.c",[]);
-exec loader.sce
-
-[c, d] = matrix([2,2;2,2], [3,8;4,5])
